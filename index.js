3374c0b187e65e90f76b29ac45e9ac0a7462d25c
nikkard1 <daniel.nikkari@aalto.fi>
2022-11-25 15:54:43 +0200

    Finished phonebook part3 steps 1 to 3

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const app = express()
+
+let phonebook = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebook)
+})
+
+app.get("/info", (request, response) => {
+  const phonebookSize = phonebook.length
+  response.end(
+    `Phonebook has info for ${phonebookSize} people\n${Date()}`
+  )
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = phonebook.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
