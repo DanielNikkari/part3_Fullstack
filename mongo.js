6e585548421ffb4e8255c927dd63aa0369e0503e
nikkard1 <daniel.nikkari@aalto.fi>
2022-11-28 22:11:35 +0200

    Finished part3 Exercice 3.12. Command-line database

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://Daniel:${password}@cluster0.iop0eh1.mongodb.net/test`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length > 3) {
+  mongoose.connect(url).then((result) => {
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4]
+    })
+    return person.save()
+  }).then(() => {
+    console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+    return mongoose.connection.close()
+  })
+  .catch((err) => {console.log(err)})
+} else {
+  mongoose.connect(url).then((result) => {
+    Person.find({}).then((persons) => {
+      console.log("phonebook:")
+      persons.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })})
+    .catch((err) => {console.log(err)})
+}
